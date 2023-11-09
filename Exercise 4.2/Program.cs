f8936a613152344a8624cab4e1e9ae2dd3bec301
John Richard Espedal <117681128+Johnricharde@users.noreply.github.com>
2023-11-09 18:26:09 +0100

    Add project files.

---
+++ Exercise 4.2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Exercise_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int var1, var2;
+
+            Console.WriteLine("Give me a number:");
+            var1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give me another one:");
+            var2 = Convert.ToInt32(Console.ReadLine());
+
+            if ( (var1 > 10) ^ (var2 > 10) )
+            {
+                Console.WriteLine($"Here are your numbers back: {var1}, {var2}." +
+                    "Thank you, come again.");
+            }
+            else
+            {
+                Console.WriteLine("Sorry, seems both numbers were either below 10 or both were over 10.");    
+            }
+
+        }
+    }
+}
